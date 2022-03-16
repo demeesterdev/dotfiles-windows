7460e31d1332ebc2d941f06c3d9e69cecba04148
tdemeester <git@demeester.dev>
2022-03-16 17:01:54 +0100

    add elevated test

--- psprofile/components.ps1
+++ psprofile/components.ps1
@@ -1,3 +1,4 @@
 # These components will be loaded for all PowerShell instances
 
+. (join-path (join-path $PSScriptRoot "components") 'console.ps1')
 . (join-path (join-path $PSScriptRoot "components") 'coreaudio.ps1')
