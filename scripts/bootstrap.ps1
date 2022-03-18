96f08a022e051f930f57b5a557c7bf5a5124ae5a
tdemeester <git@demeester.dev>
2022-03-18 11:48:43 +0100

    fix pwsh bootstrapping

--- scripts/bootstrap.ps1
+++ scripts/bootstrap.ps1
@@ -5,15 +5,25 @@ $componentSourceDir = join-path $profileSourceDir 'components'
 $dotfilesSourceDir = join-path $dotfilesPath 'dotfiles'
 
 
-$profileDir = Split-Path -parent $profile
-$componentDir = Join-Path $profileDir "components"
+$currentProfileDir = Split-Path -parent $profile
+$ProfileDirs = @()
+$profileDirs += $currentProfileDir
+if ((split-path -Leaf $profileDir) -eq 'WindowsPowershell') {
+    $ProfileDirs += join-path (split-path -Parent $profileDir) 'Powershell'
+}
+foreach ($profileDir in $ProfileDirs) {
+    $componentDir = Join-Path $profileDir "components"
 
-New-Item $profileDir -ItemType Directory -Force -ErrorAction SilentlyContinue | Out-Null
-New-Item $componentDir -ItemType Directory -Force -ErrorAction SilentlyContinue | Out-Null
+    New-Item $profileDir -ItemType Directory -Force -ErrorAction SilentlyContinue | Out-Null
+    New-Item $componentDir -ItemType Directory -Force -ErrorAction SilentlyContinue | Out-Null
 
-Copy-Item -Path (join-path $profileSourceDir '*.ps1') -Destination $profileDir
-Copy-Item -Path (join-path $componentSourceDir '**') -Destination $componentDir -Include **
-Copy-Item -Path (join-path $dotfilesSourceDir '**') -Destination $home -Include **
+    Copy-Item -Path (join-path $profileSourceDir '*.ps1') -Destination $profileDir
+    Copy-Item -Path (join-path $componentSourceDir '**') -Destination $componentDir -Include **
+    Copy-Item -Path (join-path $dotfilesSourceDir '**') -Destination $home -Include **
 
+}
+Remove-Variable CurrentProfileDir
+Remove-Variable ProfileDirs
+Remove-Variable ProfileDir
 Remove-Variable componentDir
-Remove-Variable profileDir
+
