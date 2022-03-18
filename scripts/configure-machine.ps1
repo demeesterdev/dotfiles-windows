16bd2e11ce1f8cda9a3a9650a73246d9958f20c6
tdemeester <git@demeester.dev>
2022-03-18 11:14:04 +0100

    update configure script

--- scripts/configure-machine.ps1
+++ scripts/configure-machine.ps1
@@ -12,3 +12,14 @@ $ConfigureMachineScriptFolder = join-path $PSScriptRoot 'configure-machine'
 . (Join-Path $ConfigureMachineScriptFolder 'configure-os.ps1')
 . (Join-Path $ConfigureMachineScriptFolder 'configure-privacy.ps1')
 . (Join-Path $ConfigureMachineScriptFolder 'configure-deps.ps1')
+
+Write-Information "===================================="
+Write-Information ""
+Write-Information "   Machine Configuration Finished"
+Write-Information ""
+Write-Information "   Some changes might require a reboot"
+Write-Information "   This script can initate a reboot if wanted:"
+$rebootChoice = Read-Host "Reboot [Y[es]: reboot](default: exit script)"
+if ($rebootChoice -eq 'Y' -or $rebootChoice -eq 'Yes'){
+    Restart-Computer 
+}
