97cce0a38f819d63da70d502c7bdf27897c1e65e
tdemeester <git@demeester.dev>
2022-03-30 09:15:19 +0200

    add wsl commaands

--- psprofile/functions/Update-WSLNameServer.ps1
+++ psprofile/functions/Update-WSLNameServer.ps1
@@ -17,7 +17,7 @@ function Update-WSLNameServer {
     switch ($Distribution) {
         'Debian' { $DistExecutable = 'debian.exe' }
         'Ubuntu' { $DistExecutable = 'ubuntu.exe' }
-        'Ubuntu-20.04' { $PackageName = 'ubuntu2004.exe' }
+        'Ubuntu-20.04' { $DistExecutable = 'ubuntu2004.exe' }
         default {
             write-error ('Distribution {0} not configured in powershell function Initialize-WSLDistribution.' -f $Distribution) -ErrorAction 'Stop'
         }
@@ -28,43 +28,58 @@ function Update-WSLNameServer {
 
     $hasWSLCOnf = Invoke-WSLCommand -DistributionName $Distribution -user 'root' -Command 'if [[ -f /etc/wsl.conf ]]; then echo 1 ; else echo 0 ; fi'
     write-verbose ("has wslconf: $hasWSLConf")
+    $editedWSLConf = $false
     if ($hasWSLCOnf -eq '1') {
         $HasREsolveConfFalse = Invoke-WSLCommand -DistributionName $Distribution -user 'root' -Command "if [[ '`$(grep 'generateResolvConf = false' /etc/wsl.conf)' != '' ]]; then echo 1 ; else echo 0 ; fi"
         write-verbose ("HasREsolveConfFalse: $HasREsolveConfFalse")
         if ($HasREsolveConfFalse -eq '0') {
-
-            $HasREsolveConfFalse = Invoke-WSLCommand -DistributionName $Distribution -user 'root' -Command "echo '[network]' >> /etc/wsl.conf"
-            $HasREsolveConfFalse = Invoke-WSLCommand -DistributionName $Distribution -user 'root' -Command "echo 'generateResolvConf = false' >> /etc/wsl.conf"
+            $command = "echo '[network]' >> /etc/wsl.conf;"
+            $command += "echo 'generateResolvConf = false' >> /etc/wsl.conf;"
+            Invoke-WSLCommand -DistributionName $Distribution -user 'root' -Command $command
+            $editedWSLConf = $true
         }
     }
     else {
-        $HasREsolveConfFalse = Invoke-WSLCommand -DistributionName $Distribution -user 'root' -Command "echo '[network]' > /etc/wsl.conf"
-        $HasREsolveConfFalse = Invoke-WSLCommand -DistributionName $Distribution -user 'root' -Command "echo 'generateResolvConf = false' >> /etc/wsl.conf"
+        $command = "echo '[network]' > /etc/wsl.conf;"
+        $command += "echo 'generateResolvConf = false' >> /etc/wsl.conf;"
+        Invoke-WSLCommand -DistributionName $Distribution -user 'root' -Command $command
+        $editedWSLConf = $true
     }
-
-    $IPConfigs = Get-NetIPConfiguration 
-    $defaultrouteInterfaceIndex = (Get-netroute -DestinationPrefix '0.0.0.0/0').ifIndex
-    $FirstConfig = $IPConfigs.where({ $_.InterfaceIndex -eq $defaultrouteInterfaceIndex })
-    $restConfigs = $IPConfigs.where({ $_.InterfaceIndex -ne $defaultrouteInterfaceIndex })
-    $sortedConfigs = @($FirstConfig) + @($restConfigs)
+    if($editedWSLConf){
+        Stop-WslDistribution -Name $Distribution
+    }
+    write-verbose 'getDNSAdresses'
+    $DNSServerAdresses = get-ciminstance -Namespace 'ROOT/StandardCimv2' -ClassName 'MSFT_DNSClientServerAddress'
+    write-verbose 'getnetroute'
+    $defaultrouteInterfaceIndex = (get-ciminstance -Namespace ROOT/StandardCimv2 -ClassName MSFT_NetRoute -Filter 'DestinationPrefix = "0.0.0.0/0"').ifIndex
+    $nameservers = @()
+    $nameservers += $DNSServerAdresses.where({$_.InterfaceIndex -eq $defaultrouteInterfaceIndex }).ServerAddresses
+    $nameservers += $DNSServerAdresses.where({$_.InterfaceIndex -ne $defaultrouteInterfaceIndex }).ServerAddresses
     
-    $nameservers = $sortedConfigs.DNSServer.ServerAddresses |
-    where { $_ -notin @(
+    $nameservers = $nameservers |
+        Where-Object { $_ -notin @(
             'fec0:0:0:ffff::1'
             'fec0:0:0:ffff::2'
             'fec0:0:0:ffff::3'
-        ) }
+        ) } |
+        select-object -Unique 
+    
     $nameservers = @($nameservers) + $FallBackNameServers | select-object -Unique 
+    
+    Write-Verbose 'delete resolv.conf if it is a link'
+    $command = "if [[ -L '/etc/resolv.conf' ]] ; then rm /etc/resolv.conf; fi;"
+    Write-Verbose 'create resolv.conf if not exists'
+    $command += "if [[ ! -f '/etc/resolv.conf' ]] ; then touch /etc/resolv.conf; fi;"
     Write-Verbose 'removing previous nameserver entries'
-    $command = "sed -i 's/^nameserver/# nameserver/g' /etc/resolv.conf"
+    write-verbose $command
     Invoke-WSLCommand -DistributionName $Distribution -user 'root' -Command ($command) 
-
+ 
+    $command = "sed -i 's/^nameserver/# nameserver/g' /etc/resolv.conf;"
     foreach ($server in $nameservers) {
         Write-Verbose ('adding dnsserver {0} to resolve.conf' -f $server)
-        $command = "sed -i '/^# nameserver {0}/d' /etc/resolv.conf;" -f $server
+        $command += "sed -i '/^# nameserver {0}/d' /etc/resolv.conf;" -f $server
         $command += "echo 'nameserver {0}' >> /etc/resolv.conf;" -f $server
-        write-verbose $command
-        Invoke-WSLCommand -DistributionName $Distribution -user 'root' -Command $command
     }
-
+    write-verbose $command
+    Invoke-WSLCommand -DistributionName $Distribution -user 'root' -Command $command    
 }
