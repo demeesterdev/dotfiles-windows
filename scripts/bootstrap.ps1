836074036d01e54c3150c400bca7461d1cfc71fd
tdemeester <git@demeester.dev>
2022-04-21 09:32:05 +0200

    fix bootstrap script for encoding

--- scripts/bootstrap.ps1
+++ scripts/bootstrap.ps1
@@ -28,18 +28,27 @@ $loaderContent = @"
 Invoke-Expression ". '$profileloader'"
 "@
 
+$loaderBlock = @"
+
+$GroupStartLine
+$loaderContent
+$GroupEndline
+"@ 
+
 foreach ($profileDir in $ProfileDirs) {
     $psprofilefile = join-path $profileDir 'profile.ps1'
-    $PSProfile = get-content $psprofilefile -Raw -ErrorAction 'silentlycontinue' 
-    $autoloaderSelector = '({0}[\n\r]+)([\s\S]*)([\n\r]+{1}[\n\r]+)' -f $GroupStartLine, $GroupEndLine 
+    if(-not (Test-Path $psprofilefile -PathType Leaf)){
+        "# PSprofile Created by dotfile script" | Set-Content $psprofilefile -Force
+    }
+    $PSProfile = get-content $psprofilefile -Raw 
+    $autoloaderSelector = '[\n\r]?[\n\r]({0}[\n\r]+)([\s\S]*)([\n\r]+{1}[\n\r]+)' -f $GroupStartLine, $GroupEndLine 
     if ($PSProfile -match $autoloaderSelector) { 
-        $PSProfile = $PSProfile -replace $autoloaderSelector, ('$1{0}$3' -f ($loaderContent.replace('$', '$$')))
+        $PSProfile = $PSProfile -replace $autoloaderSelector, $loaderBlock
         set-content -Value $PSProfile -Path $psprofilefile
     }
     else {
-        $GroupStartLine >> $psprofilefile
-        $loaderContent >> $psprofilefile
-        $GroupEndLine >> $psprofilefile
+        $loaderBlock | Add-Content -Path $psprofilefile
+
     }       
 }
 
